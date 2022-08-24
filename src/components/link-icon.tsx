ea70ccde45a2b656ad3824463b842b4868bf2bb2
ivy <ivy@not-ivy.dev>
2022-08-24 08:20:44 -0700

    open external link in new tab

--- src/components/link-icon.tsx
+++ src/components/link-icon.tsx
@@ -4,7 +4,7 @@ import {HiExternalLink} from 'react-icons/hi';
 export default class LinkIcon extends Component<{href: string}> {
 	render() {
 		return (
-			<a className='inline-flex focus:text-green-400 transition-colors' href={this.props.href}>{this.props.children}<HiExternalLink /></a>
+			<a className='inline-flex focus:text-green-400 transition-colors' target='_blank' href={this.props.href}>{this.props.children}<HiExternalLink /></a>
 		);
 	}
 }
