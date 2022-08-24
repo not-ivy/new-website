c9712a5928a2639817b2a0d5de255b1d7fa6620f
ivy <ivy@not-ivy.dev>
2022-08-23 21:28:25 -0700

    make all links green when focused

--- src/router/contacts.tsx
+++ src/router/contacts.tsx
@@ -6,15 +6,15 @@ export default function Contacts() {
 		<div>
 			<span className='flex items-center'>
 				<div className='m-4'><FaGithub size={30} /></div>
-				<a className='flex' href='https://github.com/not-ivy'>not-ivy<HiExternalLink /></a>
+				<a className='flex focus:text-green-400 transition-colors' href='https://github.com/not-ivy'>not-ivy<HiExternalLink /></a>
 			</span>
 			<span className='flex items-center'>
 				<div className='m-4'><HiKey size={30} /></div>
-				<a className='flex' href='https://keys.openpgp.org/vks/v1/by-fingerprint/2377F28C8BDC3BB7CDE8337CD2A1D05E1B5EAA32'>2377F28C8BDC3BB7CDE8337CD2A1D05E1B5EAA32 <HiExternalLink /></a>
+				<a className='flex focus:text-green-400 transition-colors' href='https://keys.openpgp.org/vks/v1/by-fingerprint/2377F28C8BDC3BB7CDE8337CD2A1D05E1B5EAA32'>2377F28C8BDC3BB7CDE8337CD2A1D05E1B5EAA32 <HiExternalLink /></a>
 			</span>
 			<span className='flex items-center'>
 				<div className='m-4'><HiMail size={30} /></div>
-				<a className='flex' href='mailto:ivy@not-ivy.dev'>ivy@not-ivy.dev<HiExternalLink /></a>
+				<a className='flex focus:text-green-400 transition-colors' href='mailto:ivy@not-ivy.dev'>ivy@not-ivy.dev<HiExternalLink /></a>
 			</span>
 			<span className='flex items-center'>
 				<div className='m-4'><FaDiscord size={30} /></div>
