a88c6b8a878ab3f13889ea65541e1f916de7293b
ivy <ivy@not-ivy.dev>
2022-08-24 08:27:32 -0700

    monospace font

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,7 @@ import Projects from './router/projects';
 
 export function App() {
 	return (
-		<div className='p-4 mx-auto max-w-screen-md'>
+		<div className='p-4 mx-auto max-w-screen-md font-mono'>
 			<Breadcrumbs />
 			<hr className='border-black dark:border-white my-4'/>
 			<Routes>
