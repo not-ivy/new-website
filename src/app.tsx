9bb6d8aa6dd3603af9c70f44629d8f352e4a012e
ivy <ivy@not-ivy.dev>
2022-09-01 19:52:45 -0700

    add @tailwindcss/typography and update blog

--- src/app.tsx
+++ src/app.tsx
@@ -11,14 +11,14 @@ import Test from './router/blog/test.mdx';
 export function App() {
 	const [flagVisibility, setVisibility] = useState(false);
 	return (
-		<div className='p-4 mx-auto max-w-screen-md font-mono'>
+		<div className='p-4 mx-auto max-w-screen-md'>
 			<Breadcrumbs />
 			<hr className='border-black dark:border-white my-4'/>
 			<Routes>
 				<Route path='/' element={<Home state={flagVisibility} setState={setVisibility} />} />
-				<Route path='/projects' element={<Projects />} />
+				<Route path='/projects' element={<div className='prose prose-zinc dark:prose-invert'><Projects /></div>} />
 				<Route path='/socials' element={<Socials />} />
-				<Route path='/blog' element={<Wiki />} />
+				<Route path='/blog' element={<div className='prose prose-zinc dark:prose-invert'><Wiki /></div>} />
 				<Route path='/blog/test' element={<Test />} />
 			</Routes>
 			<div id='bottom' className={`${flagVisibility ? 'w-screen' : 'w-0'} absolute bottom-0 left-0 h-2 transition-all`}></div>
