492a1dddfe6e1d609dbc387dadb691f81e5e55d7
ivy <ivy@not-ivy.dev>
2022-11-22 14:27:33 -0800

    add unocss and remove default styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
+import { readFileSync } from 'node:fs';
 import preact from '@preact/preset-vite'
+import Unocss from 'unocss/vite'
+import presetMini from '@unocss/preset-mini'
+import presetWebFonts from '@unocss/preset-web-fonts'
+import presetIcons from '@unocss/preset-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  build: {
+    target: 'esnext',
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
+  plugins: [
+    preact(),
+    Unocss({
+      presets: [
+        presetIcons({
+          collections: {
+            heroicons: () => JSON.parse(readFileSync('./node_modules/@iconify-json/heroicons/icons.json', 'utf-8'))
+          },
+        }),
+        presetMini({ dark: 'media' }),
+        presetWebFonts({
+          provider: 'google',
+          fonts: {
+            sans: 'Manrope',
+            mono: 'Major Mono Display',
+          }
+        }),
+      ],
+      theme: {
+        colors: {
+          'palette': {
+            black: "#333335",
+            white: "#fbffd4",
+            cyan: "#aefdfe",
+            magenta: "#dfa1ff",
+            yellow: "#e6f697",
+          }
+        }
+      }
+    }),
+  ],
 })
