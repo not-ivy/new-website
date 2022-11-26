2298cb03d32bbf361ed1204f5a2e93dfb15e1c70
ivy <ivy@not-ivy.dev>
2022-11-25 21:07:13 -0800

    moved boilerplate to layout file

--- src/pages/WikiPageSection.tsx
+++ src/pages/WikiPageSection.tsx
@@ -1,5 +1,7 @@
 import ErrorSplash from "../ui/ErrorSplash";
-import { Suspense, lazy } from 'preact/compat';
+import { Suspense, lazy, useState, useEffect } from 'preact/compat';
+import { InterfaceWikiEntry } from "../types/wiki";
+import { fetchWikiMetadata, formatDateFromString } from "../utils/wiki";
 
 export interface Props {
   params: {
@@ -9,12 +11,23 @@ export interface Props {
 
 export default function WikiPageSection({ params }: Props) {
   const { page } = params;
-
+  const [locales, setLocales] = useState(['en-US']);
+  const [metadata, setMetadata] = useState<InterfaceWikiEntry | undefined | Error>(undefined);
   const WikiPage = lazy(() => import(/* @vite-ignore */ `./wiki/${page}.tsx`));
+
+  useEffect(() => {
+    setLocales([...navigator.languages]);
+    fetchWikiMetadata(page).then(setMetadata).catch(setMetadata);
+  }, []);
+
+  if (!metadata || metadata instanceof Error) return <ErrorSplash message="An error occurred." />;
 
   return (
     <Suspense fallback={<></>}>
-      <WikiPage />
+      <WikiPage
+        created={formatDateFromString(locales, metadata.created)}
+        edited={formatDateFromString(locales, metadata.edited)}
+      />
     </Suspense>
   )
 }
--- src/pages/wiki/test.tsx
+++ src/pages/wiki/test.tsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from "preact/hooks";
-import { useLocation } from "wouter";
 import { InterfaceWikiEntry } from "../../types/wiki";
-import { fetchWikiMetadata, formatDateFromString } from "../../utils/wiki";
 
-export default function TestWikiPage() {
-  const [location] = useLocation();
-  const [locales, setLocales] = useState(['en-US']);
-  const [metadata, setMetadata] = useState<InterfaceWikiEntry | undefined>(undefined);
-
-  useEffect(() => {
-    setLocales([...navigator.languages]);
-    fetchWikiMetadata('test').then(setMetadata);
-    console.log(location);
-  }, []);
-
-  if (!metadata) return <>Loading...</>;
-
+export default function TestWikiPage({ created, edited }: InterfaceWikiEntry) {
   return (
     <div>
       <h3 className="font-mono text-2xl md:text-3xl text-center mb-2">Test Wiki Page</h3>
       <div className="flex flex-col text-xs md:text-sm font-mono justify-center items-end mb-4">
-        <span>Created: {formatDateFromString(locales, metadata.created)}</span>
-        <span>Edited: {formatDateFromString(locales, metadata.edited)}</span>
+        <span>Created: {created}</span>
+        <span>Edited: {edited}</span>
       </div>
       <p className="text-base md:text-lg">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam molestias deserunt impedit nulla dicta error nisi magni, dolores, labore reiciendis facere ex, saepe accusamus soluta tenetur eum id veniam delectus.
