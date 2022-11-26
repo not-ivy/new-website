0433c8b70ff29e94efaf54de70633655ff12652b
ivy <ivy@not-ivy.dev>
2022-11-25 19:23:06 -0800

    change work in progress splash

--- src/ui/WorkInProgress.tsx
+++ src/ui/WorkInProgress.tsx
@@ -1,10 +1,10 @@
-import WarningTriangle from "./icons/WarningTriangle";
+import Pencil from "./icons/Pencil";
 
 export default function WorkInProgress() {
   return (
-    <div className="border-4 border-dashed border-palette-magenta text-center p-4 mb-8">
-      <div className="mb-2 flex items-end justify-center">
-        <WarningTriangle />
+    <div className="border-4 border-dashed border-yellow text-center p-4 mb-8">
+      <div className="mb-2 flex items-center justify-center gap-x-4">
+        <Pencil className="w-6 h-6" />
         <h3 className="font-mono text-3xl">Warning</h3>
       </div>
       <p>
